9e55e5474b611586fd1296e281f22114a5322f40
Daniele <daniele.pau@teoresigroup.com>
2018-10-26 17:44:09 +0200

    angular tour of heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// Auto imported
 import { AppComponent } from './app.component';
+import { HerosComponent } from './heros/heros.component';
+
+// Import FormsModule, ngModel lives here
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
+  // every component MUST be declared here
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
